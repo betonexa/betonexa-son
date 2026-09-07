0a71cab4e99d5b3532b2e3d1e064bbc2d69de65a
betonexa <evreninsaat.ozlem@gmail.com>
2026-09-07 07:27:19 -0700

    Sevkiyat tarih filtresi düzeltmesini yükle

--- tracking-finalizer.js
+++ tracking-finalizer.js
@@ -122,3 +122,11 @@
   s.defer=true;
   document.head.appendChild(s);
 })();
+
+(function loadShipmentDateFilterFix(){
+  if(document.querySelector('script[src^="./shipment-date-filter-fix.js"]'))return;
+  const s=document.createElement('script');
+  s.src='./shipment-date-filter-fix.js?v=20260907-1';
+  s.defer=true;
+  document.head.appendChild(s);
+})();
